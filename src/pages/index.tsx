137c8393facd2a0f6f0b71922d4d6d29ea75104e
Tan Lip Wei <tlipwei@gmail.com>
2023-02-11 02:29:14 +0800

    Edit homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,9 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello world</>;
+  return (
+    <div className="flex justify-center mt-52 text-9xl font-semibold">
+      HIRE 4 GOOD
+    </div>
+  );
 }
